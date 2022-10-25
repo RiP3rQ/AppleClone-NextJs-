1ce240ae9126d63ecd7b6ebd4ff4e0f57c1849f2
RiP3rQ <rafalpompa2000@gmail.com>
2022-10-25 18:48:51 +0200

    feat: implementing headlessui / section element

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -22,7 +22,7 @@ function Landing() {
           <a className="link">Learn More</a>
         </div>
       </div>
-
+      {/* image */}
       <div className="relative hidden h-[450px] w-[450px] transition-all duration-500 md:inline lg:h-[650px] lg:w-[600px]">
         <Image src="/iphone.png" layout="fill" objectFit="contain" />
       </div>
