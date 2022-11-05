2efd56c374204ce29eb6e1f061570d68390da8c9
RiP3rQ <rafalpompa2000@gmail.com>
2022-11-05 14:42:32 +0100

    feat: checkout page (NOT DONE) + checkoutProduct component DONE

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -7,8 +7,24 @@ import { useSelector } from "react-redux";
 import Button from "../components/Button";
 // import CheckoutProduct from "../components/CheckoutProduct";
 import { selectBasketItems, selectBasketTotal } from "../redux/basketSlice";
+import CheckoutProduct from "../components/CheckoutProduct";
 
 function Checkout() {
+  const items = useSelector(selectBasketItems);
+  const router = useRouter();
+  const [groupedItemsInBasket, setGroupedItemsInBasket] = useState(
+    {} as { [key: string]: Product[] }
+  );
+
+  useEffect(() => {
+    const groupedItems = items.reduce((results, item) => {
+      (results[item._id] = results[item._id] || []).push(item);
+      return results;
+    }, {} as { [key: string]: Product[] });
+
+    setGroupedItemsInBasket(groupedItems);
+  }, [items]);
+
   return (
     <div>
       <Head>
@@ -20,8 +36,27 @@ function Checkout() {
 
       <main>
         <div>
-          <h1>{}</h1>
+          <h1 className="my-4 text-3xl font-semibold lg:text-4xl">
+            {items.length > 0 ? "Review your bag." : "Your bag is empty."}
+          </h1>
+
+          <p className="my-4">Free delivery and free returns.</p>
+
+          {items.length === 0 && (
+            <Button
+              title="Continue Shopping"
+              onClick={() => router.push("/")}
+            />
+          )}
         </div>
+
+        {items.length > 0 && (
+          <div>
+            {Object.entries(groupedItemsInBasket).map(([key, items]) => (
+              <CheckoutProduct key={key} items={items} id={key} />
+            ))}
+          </div>
+        )}
       </main>
     </div>
   );
