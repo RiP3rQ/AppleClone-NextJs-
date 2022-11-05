be386500c30212b2e79a1e6172399aed58964916
RiP3rQ <rafalpompa2000@gmail.com>
2022-11-05 13:13:52 +0100

    feat: redux setup

--- components/Product.tsx
+++ components/Product.tsx
@@ -8,6 +8,8 @@ interface Props {
 }
 
 function Product({ product }: Props) {
+  const addItemToBasket = () => {};
+
   return (
     <div className="flex h-fit w-[320px] select-none flex-col space-y-3 rounded-xl bg-[#35383C] p-8 md:h-[500px] md:w-[400px] md:p-10">
       <div className="relative h-64 w-full md:h-72">
@@ -25,7 +27,10 @@ function Product({ product }: Props) {
           <p>$ {product.price}</p>
         </div>
 
-        <div className="flex h-16 w-16 flex-shrink-0 cursor-pointer items-center justify-center rounded-full bg-gradient-to-r from-pink-500 to-violet-500 md:h-[70px] md:w-[70px]">
+        <div
+          className="flex h-16 w-16 flex-shrink-0 cursor-pointer items-center justify-center rounded-full bg-gradient-to-r from-pink-500 to-violet-500 md:h-[70px] md:w-[70px]"
+          onClick={addItemToBasket}
+        >
           <ShoppingCartIcon className="h-8 w-8 text-white" />
         </div>
       </div>
