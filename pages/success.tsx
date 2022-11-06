9f8029ee1a142f9baf8f034c66ac385f141284a5
RiP3rQ <rafalpompa2000@gmail.com>
2022-11-06 21:07:19 +0100

    feat: finishing successful checkout page

--- pages/success.tsx
+++ pages/success.tsx
@@ -151,8 +151,64 @@ function Success({ products }: Props) {
             </div>
 
             {showOrderSummaryCondition && (
-              <div>
-                <div></div>
+              <div className="mx-auto max-w-xl divide-y border-gray-300 px-4 py-4 lg:mx-0 lg:max-w-lg lg:px-10 lg:py-16">
+                <div className="space-y-4 pb-4">
+                  {products.map((product) => (
+                    <div
+                      key={product.id}
+                      className="flex items-center space-x-4 text-sm font-medium"
+                    >
+                      <div className="relative flex h-16 w-16 items-center justify-center rounded-md border border-gray-300 bg-[#F1F1F1] text-xs text-white">
+                        <div className="relative h-7 w-7 animate-bounce rounded-md">
+                          <Image
+                            src="https://rb.gy/vsvv2o"
+                            layout="fill"
+                            objectFit="contain"
+                          />
+                        </div>
+                        <div className="absolute -right-2 -top-2 flex h-5 w-5 items-center justify-center rounded-full bg-[gray] text-xs">
+                          {product.quantity}
+                        </div>
+                      </div>
+                      <p className="flex-1">{product.description}</p>
+                      <p>
+                        <Currency
+                          quantity={product.price.unit_amount / 100}
+                          currency={product.currency}
+                        />
+                      </p>
+                    </div>
+                  ))}
+                </div>
+
+                <div className="space-y-1 py-4">
+                  <div className="flex justify-between text-sm">
+                    <p className="text-[gray]">Subtotal</p>
+                    <p className="font-medium">
+                      <Currency quantity={subtotal} />
+                    </p>
+                  </div>
+                  <div className="flex justify-between text-sm">
+                    <p className="text-[gray]">Discount</p>
+                    <p className="text-[gray]"></p>
+                  </div>
+                  <div className="flex justify-between text-sm">
+                    <p className="text-[gray]">Shipping</p>
+                    <p className="font-medium">
+                      <Currency quantity={20} currency="USD" />
+                    </p>
+                  </div>
+                </div>
+
+                <div className="flex justify-between pt-4">
+                  <p>Total</p>
+                  <p className="flex items-center gap-x-2 text-xs text-[gray]">
+                    USD
+                    <span className="text-xl font-medium text-black">
+                      <Currency quantity={subtotal + 20} />
+                    </span>
+                  </p>
+                </div>
               </div>
             )}
           </section>
